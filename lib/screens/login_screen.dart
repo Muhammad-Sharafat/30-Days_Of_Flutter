059ac75d9dfaad610bacf2b1ba4dda6d54de8b81
Muhammad-Sharafat <154695077+Muhammad-Sharafat@users.noreply.github.com>
2024-02-17 22:52:22 +0500

    day5 completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,55 +2,69 @@
 // import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
+import 'package:thirty_days_flutter/utils/routes.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/login.png',
-            fit: BoxFit.contain,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            'Welcome',
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Username', hintText: 'Enter username'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Password', hintText: 'Enter password'),
-                  obscureText: true,
-                ),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/login.png',
+              fit: BoxFit.contain,
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Log In'),
-            // style: ButtonStyle(),
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Username', hintText: 'Enter username'),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Password', hintText: 'Enter password'),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.HomeRoute);
+              },
+              // style: TextButton.styleFrom(minimumSize: Size(300, 40), backgroundColor: Colors.deepPurple, ),
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.deepPurple,
+                  fixedSize: const Size(300, 30)),
+              child: const Text('Log In'),
+            )
+          ],
+        ),
       ),
     );
   }
